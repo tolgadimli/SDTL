029023d33b43d5f22cbcd7c0deb2699f055d820f
tolga <td2249@nyu.edu>
2024-01-10 18:25:10 -0500

    duplicate file fix

--- DistOptimizers.py
+++ DistOptimizers.py
@@ -1,48 +1,19 @@
 import torch
 import torch.distributed as dist
-import torch.utils.data as udata
-
-def sync_model_params_and_buffers(model):
-    ''' synchronize model params and buffers '''
-    current_index = 0
-    for parameter in model.parameters():
-        numel = parameter.data.numel()
-        dist.all_reduce_multigpu([parameter.data], op=torch.distributed.ReduceOp.AVG, async_op=False)
-        current_index += numel
-
-    current_index = 0
-    for parameter in model.buffers():
-        numel = parameter.data.numel()
-        dist.all_reduce_multigpu([parameter.data], op=torch.distributed.ReduceOp.AVG, async_op=False)
-        current_index += numel
-
-def to_dist_train_loader(train_loader, is_dist_sampler=False):
-    ''' convert local train data loader to distributed train data loader by modifying the arguments '''
-    if is_dist_sampler:
-        train_sampler = udata.distributed.DistributedSampler(train_loader.dataset, shuffle=True, drop_last=train_loader.drop_last)
-        return udata.DataLoader(dataset=train_loader.dataset,
-                                batch_size=train_loader.batch_size,
-                                num_workers=train_loader.num_workers,
-                                pin_memory=True, persistent_workers=True, sampler = train_sampler)
-    else:
-        return udata.DataLoader(dataset=train_loader.dataset,
-                                drop_last=train_loader.drop_last,
-                                batch_size=train_loader.batch_size,
-                                num_workers=train_loader.num_workers,
-                                pin_memory=True, persistent_workers=True, shuffle=True)
 
 class CommOptimizer:
-    def __init__(self, local_optimizer, dist_optim_name, local_pulling_strength, dist_pulling_strength, comm_period):
+    def __init__(self, local_optimizer, dist_optim_name, world_size, local_pulling_strength, dist_pulling_strength, comm_period):
         ''' combine both local training and distributed training optimziers '''
         self.local_optimizer = local_optimizer
+
         if dist_optim_name == 'LSGD':
-            self.dist_optimizer = LSGD(local_optimizer, local_pulling_strength, dist_pulling_strength, comm_period)
-        if dist_optim_name == 'LSGD+':
-            self.dist_optimizer = LSGDPlus(local_optimizer, local_pulling_strength, dist_pulling_strength, comm_period)
+            self.dist_optimizer = LSGD(local_optimizer, world_size, local_pulling_strength, dist_pulling_strength, comm_period)
+        elif dist_optim_name == 'LSGD+':
+            self.dist_optimizer = LSGDPlus(local_optimizer, world_size, local_pulling_strength, dist_pulling_strength, comm_period)
         elif dist_optim_name == 'EASGD':
-            self.dist_optimizer = EASGD(local_optimizer, local_pulling_strength, dist_pulling_strength, comm_period)
+            self.dist_optimizer = EASGD(local_optimizer, world_size, local_pulling_strength, dist_pulling_strength, comm_period)
         elif dist_optim_name == 'EASGD+':
-            self.dist_optimizer = EASGDPlus(local_optimizer, local_pulling_strength, dist_pulling_strength, comm_period)
+            self.dist_optimizer = EASGDPlus(local_optimizer, world_size, local_pulling_strength, dist_pulling_strength, comm_period)
 
     def zero_grad(self):
         self.local_optimizer.zero_grad()
@@ -53,12 +24,14 @@ class CommOptimizer:
             self.dist_optimizer.step()
 
 class DistOptimizer:
-    def __init__(self, local_optimizer, local_pulling_strength, dist_pulling_strength, comm_period):
-        self.dist_counter = 0
+    def __init__(self, local_optimizer, world_size, local_pulling_strength, dist_pulling_strength, comm_period):
+        
+        self.dist_counter = 1
         self.comm_period = comm_period
-        self.local_pulling_strength = local_pulling_strength
-        self.dist_pulling_strength = dist_pulling_strength
+        self.local_pulling_strength = local_pulling_strength # p
+        self.dist_pulling_strength = dist_pulling_strength # c
         self.local_optimizer = local_optimizer
+        self.world_size = world_size
         
         self.param_tensor_list = []
         self.center_tensor_list = []
@@ -76,12 +49,15 @@ class DistOptimizer:
             self.center_tensor_list += [param_tensor.clone()]
             
     def local_pulling_step(self):
-        p = self.local_pulling_strength / self.comm_period
-        for param_group, center_tensor in zip(self.local_optimizer.param_groups, self.center_tensor_list):
-            counter = 0
-            for param in param_group['params']:
-                param.data.mul_(1-p).add_(center_tensor[counter:counter+param.numel()].view_as(param), alpha=p)
-                counter = counter + param.numel()
+        if self.local_pulling_strength > 0:
+            p = self.local_pulling_strength / self.comm_period
+            for param_group, center_tensor in zip(self.local_optimizer.param_groups, self.center_tensor_list):
+                counter = 0
+                for param in param_group['params']:
+                    param.data.mul_(1-p).add_(center_tensor[counter:counter+param.numel()].view_as(param), alpha=p)
+                    counter = counter + param.numel()
+        else:
+            pass
 
     def step(self):
         self.dist_counter += 1
@@ -89,7 +65,7 @@ class DistOptimizer:
         self.dist_train_step()
         if self.dist_counter % self.comm_period == 0: # dist training
             self.comm_train_step()
-            self.dist_counter = 0
+            self.dist_counter = 1
     
     def comm_train_step(self):
         raise NotImplementedError
@@ -97,9 +73,44 @@ class DistOptimizer:
     def dist_train_step(self): 
         raise NotImplementedError
     
+
+class EASGD(DistOptimizer):
+    def __init__(self, local_optimizer, world_size, local_pulling_strength, dist_pulling_strength, comm_period):
+        super().__init__(local_optimizer, world_size, local_pulling_strength, dist_pulling_strength, comm_period)
+        self.dist_message_tensor_list = []
+        self.dist_loss_tensor_list = []
+        # self.dist_pulling_strength is the Beta term
+        self.alpha = self.dist_pulling_strength/world_size
+        print(self.alpha)
+        print(self.dist_pulling_strength)
+
+    def dist_train_step(self):
+        pass
+
+    def comm_train_step(self):
+        for param_group, param_tensor, center_tensor in zip(self.local_optimizer.param_groups, self.param_tensor_list, self.center_tensor_list):
+            # find average of model parameters
+            counter = 0
+            for param in param_group['params']:
+                param_tensor[counter:counter+param.numel()].copy_(param.view(-1))
+                counter += param.numel()
+            dist.all_reduce_multigpu([param_tensor], op=torch.distributed.ReduceOp.AVG, async_op=False)
+
+            # update model parameters
+            counter = 0
+            for param in param_group['params']:
+                param.data.mul_(1-self.alpha)
+                param.data.add_(center_tensor[counter:counter+param.numel()].view_as(param), alpha=self.alpha)
+                counter += param.numel()
+
+            # center_tensor.copy_(param_tensor) => EASGD also takes average with respect to time so in that sense this does not capture it appropriately
+            center_tensor.mul_(1-self.dist_pulling_strength) 
+            center_tensor.add_(param_tensor, alpha = self.dist_pulling_strength)
+
+
 class LSGD(DistOptimizer):
-    def __init__(self, local_optimizer, local_pulling_strength, dist_pulling_strength, comm_period):
-        super().__init__(local_optimizer, local_pulling_strength, dist_pulling_strength, comm_period)
+    def __init__(self, local_optimizer, world_size, local_pulling_strength, dist_pulling_strength, comm_period):
+        super().__init__(local_optimizer, world_size, local_pulling_strength, dist_pulling_strength, comm_period)
         self.dist_message_tensor_list = []
         self.dist_loss_tensor_list = []
         for device in self.device_list:
@@ -107,13 +118,15 @@ class LSGD(DistOptimizer):
             self.dist_loss_tensor_list += [torch.zeros(1, device=device)]
 
     def dist_train_step(self):
+        """I could not understand what exactly this step is doing for LSGD. Are you approximating the train loss value with first order taylor expansion?"""
         # copy model parameters into dist tensor
         for param_group, param_tensor, loss in zip(self.local_optimizer.param_groups, self.param_tensor_list, self.dist_loss_tensor_list):
             counter = 0; group_loss = 0
             for param in param_group['params']:
                 with torch.no_grad():
-                    cur_step = param_tensor[counter:counter+param.data.numel()] - param.data.view(-1)
-                    group_loss += (cur_step * param.grad.view(-1)).sum().item()
+                    cur_step = param_tensor[counter:counter+param.data.numel()] - param.data.view(-1) # delta_x = learning rate * grad
+                    group_loss += (cur_step * param.grad.view(-1)).sum().item() # f'(x) * delta_x
+                    print(group_loss)
                 param_tensor[counter:counter+param.numel()].copy_(param.view(-1))
                 counter += param.numel()
             loss.mul_(0.9).add_(group_loss, alpha=0.1)
@@ -137,70 +150,11 @@ class LSGD(DistOptimizer):
                         counter += param.numel()
                     center_tensor.copy_(param_tensor)
 
-class LSGDPlus(LSGD):
-    def __init__(self, local_optimizer, local_pulling_strength, dist_pulling_strength, comm_period):
-        super().__init__(local_optimizer, local_pulling_strength, dist_pulling_strength, comm_period)
 
-    def comm_train_step(self):
-        for param_group, param_tensor, center_tensor, message, loss in \
-            zip(self.local_optimizer.param_groups, self.param_tensor_list, self.center_tensor_list, \
-                self.dist_message_tensor_list, self.dist_loss_tensor_list):
-                    # find the average of workers
-                    center_tensor.copy_(param_tensor)
-                    dist.all_reduce_multigpu([center_tensor], op=torch.distributed.ReduceOp.AVG, async_op=False)
-
-                    # find the rank of leader
-                    message.zero_(); message[dist.get_rank()] = loss.item()
-                    dist.all_reduce_multigpu([message], op=torch.distributed.ReduceOp.SUM, async_op=False)
-                    leader_rank = message.argmin().item()
-                    # if dist.get_rank() == leader_rank:
-                    #     m_weight = 1 + self.dist_pulling_strength * (dist.get_world_size() - 1)
-                    # else:
-                    #     m_weight = 1 - self.dist_pulling_strength
-                    # param_tensor = m_weight * param_tensor
-                    # dist.all_reduce_multigpu([param_tensor], op=torch.distributed.ReduceOp.AVG, async_op=False)
-                    dist.broadcast_multigpu([param_tensor], src=leader_rank, async_op=False)
-                    delta_tensor = self.dist_pulling_strength * (param_tensor - center_tensor)
-                    center_tensor.add_(delta_tensor)
-
-                    # update model parameters
-                    counter = 0
-                    for param in param_group['params']:    
-                        param.data.add_(delta_tensor[counter:counter+param.numel()].view_as(param))
-                        param.data.mul_(1-self.dist_pulling_strength)
-                        param.data.add_(center_tensor[counter:counter+param.numel()].view_as(param), alpha=self.dist_pulling_strength)
-                        param_tensor[counter:counter+param.numel()].copy_(param.data.view(-1))
-                        counter += param.numel()
-
-class EASGD(DistOptimizer):
-    def __init__(self, local_optimizer, local_pulling_strength, dist_pulling_strength, comm_period):
-        super().__init__(local_optimizer, local_pulling_strength, dist_pulling_strength, comm_period)
-        self.dist_message_tensor_list = []
-        self.dist_loss_tensor_list = []
-
-    def dist_train_step(self):
-        pass
-
-    def comm_train_step(self):
-        for param_group, param_tensor, center_tensor in zip(self.local_optimizer.param_groups, self.param_tensor_list, self.center_tensor_list):
-            # find average of model parameters
-            counter = 0
-            for param in param_group['params']:
-                param_tensor[counter:counter+param.numel()].copy_(param.view(-1))
-                counter += param.numel()
-            dist.all_reduce_multigpu([param_tensor], op=torch.distributed.ReduceOp.AVG, async_op=False)
-
-            # update model parameters
-            counter = 0
-            for param in param_group['params']:
-                param.data.mul_(1-self.dist_pulling_strength)
-                param.data.add_(center_tensor[counter:counter+param.numel()].view_as(param), alpha=self.dist_pulling_strength)
-                counter += param.numel()
-            center_tensor.copy_(param_tensor)
 
 class EASGDPlus(EASGD):
-    def __init__(self, local_optimizer, local_pulling_strength, dist_pulling_strength, comm_period):
-        super().__init__(local_optimizer, local_pulling_strength, dist_pulling_strength, comm_period)
+    def __init__(self, local_optimizer, world_size, local_pulling_strength, dist_pulling_strength, comm_period):
+        super().__init__(local_optimizer, world_size, local_pulling_strength, dist_pulling_strength, comm_period)
         self.grad_tensor_list = []
         for param_tensor in self.param_tensor_list:
             self.grad_tensor_list += [torch.zeros_like(param_tensor)]
@@ -240,3 +194,39 @@ class EASGDPlus(EASGD):
                     param.data.add_(center_tensor[counter:counter+param.numel()].view_as(param), alpha=self.dist_pulling_strength)
                     param_tensor[counter:counter+param.numel()].copy_(param.data.view(-1))
                     counter += param.numel()
+
+
+class LSGDPlus(LSGD):
+    def __init__(self, local_optimizer, world_size, local_pulling_strength, dist_pulling_strength, comm_period):
+        super().__init__(local_optimizer, world_size, local_pulling_strength, dist_pulling_strength, comm_period)
+
+    def comm_train_step(self):
+        for param_group, param_tensor, center_tensor, message, loss in \
+            zip(self.local_optimizer.param_groups, self.param_tensor_list, self.center_tensor_list, \
+                self.dist_message_tensor_list, self.dist_loss_tensor_list):
+                    # find the average of workers
+                    center_tensor.copy_(param_tensor)
+                    dist.all_reduce_multigpu([center_tensor], op=torch.distributed.ReduceOp.AVG, async_op=False)
+
+                    # find the rank of leader
+                    message.zero_(); message[dist.get_rank()] = loss.item()
+                    dist.all_reduce_multigpu([message], op=torch.distributed.ReduceOp.SUM, async_op=False)
+                    leader_rank = message.argmin().item()
+                    # if dist.get_rank() == leader_rank:
+                    #     m_weight = 1 + self.dist_pulling_strength * (dist.get_world_size() - 1)
+                    # else:
+                    #     m_weight = 1 - self.dist_pulling_strength
+                    # param_tensor = m_weight * param_tensor
+                    # dist.all_reduce_multigpu([param_tensor], op=torch.distributed.ReduceOp.AVG, async_op=False)
+                    dist.broadcast_multigpu([param_tensor], src=leader_rank, async_op=False)
+                    delta_tensor = self.dist_pulling_strength * (param_tensor - center_tensor)
+                    center_tensor.add_(delta_tensor)
+
+                    # update model parameters
+                    counter = 0
+                    for param in param_group['params']:    
+                        param.data.add_(delta_tensor[counter:counter+param.numel()].view_as(param))
+                        param.data.mul_(1-self.dist_pulling_strength)
+                        param.data.add_(center_tensor[counter:counter+param.numel()].view_as(param), alpha=self.dist_pulling_strength)
+                        param_tensor[counter:counter+param.numel()].copy_(param.data.view(-1))
+                        counter += param.numel()
--- main.py
+++ main.py
@@ -1,39 +1,25 @@
 import time
 import copy
-import argparse
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.distributed as dist
-from torch.optim.lr_scheduler import StepLR
+from torch.optim.lr_scheduler import StepLR, MultiStepLR
 
 import matplotlib.pyplot as plt
 import torch.optim as optim
 import torch.multiprocessing as mp
 from torchvision import datasets, transforms
 from DistOptimizers import CommOptimizer
-from DistOptimizers import to_dist_train_loader, sync_model_params_and_buffers
 
-class NeuralNetwork(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.base = nn.Sequential(
-            nn.Conv2d(  1, 16, 3, 2), nn.ReLU(),
-            nn.Conv2d( 16, 32, 3, 2), nn.ReLU(),
-            nn.Conv2d( 32, 64, 3, 2), nn.ReLU(),
-        )   
-        self.classifier = nn.Sequential(
-            nn.Linear( 256, 100), nn.ReLU(),
-            nn.Linear( 100,  10)
-        )
+from options import Options
+from utils import *
+import os
 
-    def forward(self, x):
-        x = self.base(x)
-        output = self.classifier(torch.flatten(x, 1))
-        return output
 
 def train(model, device, train_loader, optimizer, scheduler, epoch):
+    train_loss = train_accuracy = 0
     train_begin_time = time.time()
     model.train()
     try:
@@ -44,11 +30,21 @@ def train(model, device, train_loader, optimizer, scheduler, epoch):
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
         output = model(data)
+        pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+
         loss = F.cross_entropy(output, target)
         loss.backward()
         optimizer.step()
+
+        train_accuracy += pred.eq(target.view_as(pred)).sum().item()
+        train_loss += loss.item()
+
     scheduler.step()
-    return time.time() - train_begin_time
+    train_loss /= len(train_loader.sampler)
+    train_error = 100.0 * ( 1 - train_accuracy / len(train_loader.sampler) )
+    epoch_duration = time.time() - train_begin_time
+    
+    return epoch_duration, train_loss, train_error
 
 def test(model, device, test_loader):
     center_model = copy.deepcopy(model)
@@ -61,73 +57,69 @@ def test(model, device, test_loader):
             test_loss += F.cross_entropy(output, target, reduction='sum').item()  # sum up batch loss
             pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
             test_accuracy += pred.eq(target.view_as(pred)).sum().item()
-    test_loss /= len(test_loader.dataset)
-    test_accuracy /= len(test_loader.dataset)
-    return test_loss, test_accuracy
 
-def dist_run(rank, accm_workers, world_size, init_method):
-    torch.manual_seed(0)
+    test_loss /= len(test_loader.dataset)
+    test_error = 100.0 * ( 1 - test_accuracy / len(test_loader.dataset) )
+    return test_loss, test_error
+
+def dist_run(rank, args, accm_workers, world_size, init_method):
+
+    torch.manual_seed(args.seed)
     dist.init_process_group(rank=accm_workers+rank, world_size=world_size, backend='nccl', init_method=init_method)
 
-    cuda_kwargs  = {'num_workers': 4}
-    train_kwargs = {'batch_size': 128}
-    test_kwargs  = {'batch_size': 1000}
+    cuda_kwargs  = {'num_workers': args.node_gpus}
+    train_kwargs = {'batch_size': args.bs}
+    test_kwargs  = {'batch_size': args.bs}
     train_kwargs.update(cuda_kwargs)
     test_kwargs.update(cuda_kwargs)
     
-    transform=transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
-    dataset_download = dist.get_rank() % torch.cuda.device_count() == 0
-    Dataset = datasets.FashionMNIST
-    if dataset_download:
-        dataset1 = Dataset('./data', train=True, download=dataset_download)
-        dataset2 = Dataset('./data', train=False, download=dataset_download)
-    dist.barrier()
-    dataset1 = Dataset('./data', train=True, download=False, transform=transform)
-    dataset2 = Dataset('./data', train=False, download=False, transform=transform)
-    train_loader = to_dist_train_loader(torch.utils.data.DataLoader(dataset1, **train_kwargs), is_dist_sampler=True)
-    test_loader  = torch.utils.data.DataLoader(dataset2, **test_kwargs)
+    train_dataset, test_dataset = get_dataset(args.dataset)
+    train_loader = to_dist_train_loader(torch.utils.data.DataLoader(train_dataset, **train_kwargs), is_dist_sampler=True)
+    test_loader  = torch.utils.data.DataLoader(test_dataset, **test_kwargs)
 
     device = torch.device('cuda:%d'%rank)
-    base_model = NeuralNetwork().to(device)
+    base_model = get_model(args.model, args.dataset).to(device)
     for model_param in base_model.parameters():
         dist.all_reduce_multigpu([model_param.data], op=torch.distributed.ReduceOp.AVG, async_op=False)
-
-    for dist_optim_name in ('DataParallel', 'LSGD', 'LSGD+', 'EASGD', 'EASGD+', ):
-        # define optimizer
-        if not dist_optim_name == 'DataParallel':
-            model     = copy.deepcopy(base_model).to(device)
-            optimizer = optim.SGD(model.parameters(), lr = 0.2, momentum = 0.9, weight_decay=1e-5)
-            scheduler = StepLR(optimizer, step_size=1, gamma=0.9)
-            optimizer = CommOptimizer(optimizer, dist_optim_name=dist_optim_name, comm_period=4, dist_pulling_strength=0.1, local_pulling_strength=0.1)
-        else:
-            model     = nn.parallel.DistributedDataParallel(copy.deepcopy(base_model).to(device))
-            optimizer = optim.SGD(model.parameters(), lr = 0.2, momentum = 0.9, weight_decay=1e-5)
-            scheduler = StepLR(optimizer, step_size=1, gamma=0.9)
+    
+    print(args.dist_optim_name)
+    if not args.dist_optim_name == 'DataParallel':
+        model     = copy.deepcopy(base_model).to(device)
+        optimizer = optim.SGD(model.parameters(), lr = args.lr, momentum = args.mom, weight_decay=args.wd, nesterov=True)
+        scheduler = MultiStepLR(optimizer, milestones=args.milestones, gamma=args.scheduler_gamma)
+        optimizer = CommOptimizer(optimizer, dist_optim_name=args.dist_optim_name, world_size = world_size,
+                                        comm_period=args.comm_period, dist_pulling_strength=args.c, local_pulling_strength=args.p)
+    else:
+        model     = nn.parallel.DistributedDataParallel(copy.deepcopy(base_model).to(device))
+        optimizer = optim.SGD(model.parameters(), lr = args.lr, momentum = args.mom, weight_decay=args.wd, nesterov=True)
+        scheduler = MultiStepLR(optimizer, milestones=args.milestones, gamma=args.scheduler_gamma)
         
-        # train model
-        total_seconds = [0]; all_test_loss = []; all_test_accuracy = []
-        for epoch in range(1, 20 + 1):
-            train_seconds = train(model, device, train_loader, optimizer, scheduler, epoch)
-            total_seconds += [train_seconds + total_seconds[-1]]
-            test_loss, test_accuracy = test(model, device, test_loader)
-            all_test_loss += [test_loss]; all_test_accuracy += [test_accuracy]
-            if dist.get_rank() % torch.cuda.device_count() == 0:
-                print('%s (epoch: %d | seconds: %.2f) te_loss: %.3f te_acc: %.3f%%'\
-                    %(dist_optim_name, epoch, total_seconds[-1], test_loss, 100. * test_accuracy))
+    # train model
+    message_file = open(args.message_dir, "w")
+    message_file.write("epoch,rank,train_time,train_loss,train_error,test_loss,test_error,lr")
+    message_file.close()
+   
+    total_train_time = 0
+    for epoch in range(1, args.epochs + 1):
+        epoch_time, train_loss, train_error = train(model, device, train_loader, optimizer, scheduler, epoch)
+        test_loss, test_error = test(model, device, test_loader)
+        cur_lr = scheduler.get_last_lr()[-1]
+        total_train_time += epoch_time 
 
-        # plot resutls
-        plt.plot(range(1, len(all_test_accuracy) + 1), [acc * 100. for acc in all_test_accuracy], label=dist_optim_name, marker='.',linewidth=2)
-        plt.xticks(range(1, len(all_test_accuracy) + 1)); plt.legend(); plt.grid(True); 
-        plt.xlabel('Epochs'); plt.ylabel('Accuracy (%)'); plt.title('Test Accuracy')
-        plt.savefig('test_results.jpg')
+        message_file = open(args.message_dir, "a")
+        _text = "\n%d,%d,%.3f,%.4f,%.3f,%.4f,%.3f,%.6f"\
+        %(epoch, rank, total_train_time, train_loss, train_error, test_loss, test_error, cur_lr) 
+        message_file.write(_text)
+        message_file.close()
+
+    if args.save_model:
+        print("Saving model parameters.")
+        state = model.state_dict()
+        torch.save(state, os.path.join(args.export_dir, args.experiment_name,'model_params-end-w%d.pt'%rank))
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--ip_address', type=str, required=True)
-    parser.add_argument('--node_rank',  type=int, default = 0) 
-    parser.add_argument('--node_gpus',  type=int, default = 100) 
-    parser.add_argument('--num_nodes', type=int, default = 1)
-    args = parser.parse_args()
+
+    args = Options().parse()
 
     cur_workers   = min(args.node_gpus, torch.cuda.device_count())
     total_workers = torch.zeros(args.num_nodes).int()
@@ -135,9 +127,9 @@ if __name__ == "__main__":
     if args.num_nodes > 1:
         init_method = "tcp://{ip}:{port}0".format(ip=args.ip_address, port=2432)
         dist.init_process_group(rank=args.node_rank, world_size=args.num_nodes, backend='gloo', init_method=init_method)
-        dist.all_redcude(total_workers, op=torch.distributed.ReduceOp.SUM, async_op=False)
+        dist.all_reduce(total_workers, op=torch.distributed.ReduceOp.SUM, async_op=False)
     accm_workers = total_workers[:args.node_rank].sum().item()
     world_size   = total_workers.sum().item()
 
     init_method = "tcp://{ip}:{port}5".format(ip=args.ip_address, port=2432)
-    mp.spawn(fn=dist_run, args=(accm_workers, world_size, init_method), nprocs=cur_workers)
+    mp.spawn(fn=dist_run, args=(args, accm_workers, world_size, init_method), nprocs=cur_workers)
